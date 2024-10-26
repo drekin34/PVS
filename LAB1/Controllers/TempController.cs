350ec9c8a0f802fbc4512351fed8141bc6066e1c
drekin34 <dmitroslobodan@gmail.com>
2024-10-26 10:14:43 +0300

    first

---
+++ LAB1/Controllers/TempController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LAB1.Controllers
+{
+    public class TempController : Controller
+    {
+        public IActionResult index()
+        {
+            return View();
+        }
+		public IActionResult aboutus()
+		{
+			return View();
+		}
+	}
+}
